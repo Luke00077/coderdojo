70747f882775606eca780aa2065abd0eee9fda01
lukas <i.am.luke.c.b@gmail.com>
2021-02-03 20:14:47 -0800

    commit again

--- coderdojo/src/main/Main.java
+++ coderdojo/src/main/Main.java
@@ -1,10 +1,22 @@
 package main;
 
+import operators.Arithmetic;
+import operators.Assignment;
+import operators.Bitwise;
+import operators.Logical;
+import operators.Misc;
+import operators.Relational;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Arithmetic math = new Arithmetic();
+Assignment assign = new Assignment();
+Bitwise bit = new Bitwise();
+Logical logic = new Logical();
+Misc hello = new Misc();
+Relational relation = new Relational();
 	}
 
 }
