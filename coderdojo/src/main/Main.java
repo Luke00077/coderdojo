4569f5a16652b8c47da265e4d9a946792ef644b0
lukas <i.am.luke.c.b@gmail.com>
2021-02-10 20:27:45 -0800

    H

--- coderdojo/src/main/Main.java
+++ coderdojo/src/main/Main.java
@@ -10,13 +10,51 @@ import operators.Relational;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Arithmetic math = new Arithmetic();
+		
+Arithmetic math = new Arithmetic(); 
+
+int addition;
+
+addition = math.add(2, 3);
+int number5 = 5;
+int number4 = 4;
+
+int subtraction;
+subtraction = (number5 - number4);
+
+System.out.println(subtraction);
+
+System.out.println(addition);
+
+int multiplication;
+int number10 = 10;
+int number1 = 1;
+multiplication = (number1 * number10);
+
+System.out.println(multiplication);
+
+int division;
+int number12 = 12;
+int number2 = 2;
+division = (number12 / number2);
+
+System.out.println(division);
+
+
 Assignment assign = new Assignment();
+
 Bitwise bit = new Bitwise();
+
 Logical logic = new Logical();
+
 Misc hello = new Misc();
+
 Relational relation = new Relational();
+
+
 	}
 
-}
+	
+	}
+
+
--- coderdojo/src/operators/Arithmetic.java
+++ coderdojo/src/operators/Arithmetic.java
@@ -1,5 +1,9 @@
 package operators;
 
 public class Arithmetic {
-
+	public int add(int number1, int number2) {
+        int sum;
+        sum = number1 + number2;
+        return sum;
+	}
 }
