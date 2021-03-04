c4c16b7fbb004b3b25f47c14a43fe0ffaaa42e61
lukas <i.am.luke.c.b@gmail.com>
2021-03-03 17:26:09 -0800

    Commit

---
+++ pets/src/database/PetInfo.java
@@ -0,0 +1,194 @@
+package database;
+
+import model.Pet;
+
+public class PetInfo {
+
+	// TODO Auto-generated method stub
+
+	public Pet getPet(String breed) {
+		if (breed == "Labrador Retriever")
+
+		{
+			Pet labrador = new Pet();
+			labrador.breed = "Labrador Retriever";
+			labrador.description = "The Lab makes his 25th straight appearance as the number one breed. And why not? This family-friendly, smart, and fun-loving breed is a favorite in households across America. Their desire to please and gentle disposition also help them excel as guide dogs and search-and-rescue dogs.";
+			return labrador;
+
+		}
+		return null;
+		
+		}
+	public Pet getPet1(String breed) {
+		if (breed == "Labrador Retriever");
+		
+		{
+			Pet shepard = new Pet();
+			shepard.breed = "Labrador Retriever";
+			shepard.description = "A true dog lover’s pet, the German Shepherd Dog is intelligent and hardworking."
+					+ " They are also known to be extremely versatile and courageous."
+					+ " No wonder they are so often trained to assist people with disabilities, the police, the military, and other service organizations.";
+	}
+		
+		return null;
+		
+		
+	}
+	public Pet getPet2(String breed) {
+		if (breed == "Golden Retriever");
+		
+		{
+			Pet goldenRetriever = new Pet();
+			goldenRetriever.breed = "Labrador Retriever";
+			goldenRetriever.description = "A very athletic breed, the Golden Retriever is another family-friendly pet with a fantastic demeanor. Because they love to run and play, they’re a great fit for families with young children. Also skilled workers, Goldens are popular hunting companions, guide dogs, and search-and-rescue dogs.";
+	}
+		return null;
+		
+
+	
+	}
+	public Pet getPet3(String breed) {
+		if (breed == "Bulldog");
+		
+		{
+			Pet Bulldog  = new Pet();
+			Bulldog.breed = "Bulldog";
+			Bulldog.description = "One of the fastest-growing breeds in terms of popularity, Bulldogs are fun and loveable. They are very calm when compared with the previously mentioned breeds, and they make great companions. They don’t need a ton of exercise—they actually prefer to stay home and relax most of the time.";
+	
+		}
+	return null;
+	
+	}
+	
+	
+	public Pet getPet4(String breed) {
+		if (breed == "Beagle");
+		
+		{
+			Pet Beagle  = new Pet();
+			Beagle.breed = "Beagle";
+			Beagle.description = "Conveniently sized with short, low-maintenance coats, the Beagle is a peaceful and cheerful pet. Unlike Bulldogs, Beagles do require some exercise, so a fenced-in yard is an ideal setting for them. A hunting breed with great curiosity, these hounds enjoy exploring and keeping their noses active.";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		}
+		return null;
+}
+
+
+
+
+public Pet getPet5(String breed) {
+	if (breed == "French Bulldog");
+	
+	
+		Pet frenchBulldog  = new Pet();
+		frenchBulldog.breed = "French Bulldog"; 
+		frenchBulldog.description = "French Bulldogs have mixed demeanors—sometimes they love to run and play, and sometimes they just want to lie around and be loved. They thrive from human contact and attention, so this isn’t a breed to be kept alone for hours at a time.";
+		return null;
+		
+		
+				
+	
+	
+}
+
+public Pet getPet6(String breed) {
+	if (breed == "Yorkshire Terrier");
+	
+	
+		Pet yorkshireTerrier = new Pet();
+		yorkshireTerrier.breed = "Poodle"; 
+		yorkshireTerrier.description = "“Yorkies” are the most popular small dog breed in America. They become extremely devoted to their owners, and their small stature makes them a great choice for people who live in condos or apartments. They can be a bit feisty at times, but overall this brave and energetic breed is excellent for anyone wanting a small companion.";
+		return null;
+
+
+
+}
+
+public Pet getPet7(String breed) {
+	if (breed == "Poodle");
+	
+	
+		Pet Poodle = new Pet();
+		Poodle.breed = "Poodle"; 
+		Poodle.description = "Poodles are known for making regular appearances in dog shows. Their elegant form never goes unnoticed, but their personalities are just as charming as their looks. They’re smart, easy to train, eager to please, and lots of fun to be around.";
+		return null;
+
+}
+
+public Pet getPet8(String breed) {
+	if (breed == "Rottweiler");
+	
+	
+		Pet Rottweiler = new Pet();
+		Rottweiler.breed = "Rottweiler"; 
+		Rottweiler.description = "Large and powerful, Rottweilers are sometimes misunderstood because of their stature. They are actually very intelligent, easily trained, and willing to work. Their broad chests and muscular bodies stand out, but Rottweilers have even bigger hearts and make for devoted companions.";
+return null;
+}
+
+public Pet getPet9(String breed) {
+	if (breed == "Boxer");
+	
+	
+		Pet Boxer = new Pet();
+		Boxer.breed = "Boxer"; 
+		Boxer.description = "Boxers have endless energy and love to play. Although they can be headstrong at times, Boxers are extremely loyal to their owners. They are great with kids while also being alert and courageous.";
+return null;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
