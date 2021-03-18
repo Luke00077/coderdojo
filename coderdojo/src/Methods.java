d3b3a06ac073ddc9aea5f1c5764c73cfae73d840
lukas <i.am.luke.c.b@gmail.com>
2021-03-17 21:28:19 -0700

    commit message

---
+++ coderdojo/src/Methods.java
@@ -0,0 +1,45 @@
+
+public class Methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 10;
+		int b = 20;
+		int c = 8;
+		int d = 9;
+		int x = 1;
+		int z = 1;
+		int j = 100;
+		int k = 10;
+		
+		multiply(10, 100);
+		multiply(c,d);
+		multiply(x,z);
+		multiply(a,b);
+		divide(j,k);
+	}
+public static void multiply(int a, int b) {
+	
+	System.out.println(a * b * 10); 
+	
+	
+
+
+}
+public static void divide(int j, int k) {
+	System.out.println(j / k);
+	
+	
+}
+
+
+
+
+
+
+}
+
+
+
