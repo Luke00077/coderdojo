b8719b9b3711850193240c16dea34eec85e3a654
lukas <i.am.luke.c.b@gmail.com>
2021-03-24 20:34:36 -0700

    commit

---
+++ coderdojo/src/ifElse/IfElseStatement.java
@@ -0,0 +1,21 @@
+package ifElse;
+
+public class IfElseStatement {
+
+	public static void main(String[] args) {
+		
+int x = 9;
+if (x==9) {
+	System.out.println("Yes, x is equal to 9.");
+	
+}
+
+else {
+	System.out.println("No, x is not equal to 9");
+}
+
+}
+
+	}
+
+
