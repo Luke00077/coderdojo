f2a9667a880fc6dc2f876a1e0c24da6f55bef0fe
lukas <i.am.luke.c.b@gmail.com>
2021-03-31 15:01:39 -0700

    message

--- pets/src/client/PetDetails.java
+++ pets/src/client/PetDetails.java
@@ -1,9 +1,17 @@
 package client;
 
+import database.PetInfo;
+import model.Pet;
+
 public class PetDetails {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//    object new PetDetails 
+		PetInfo information = new PetInfo();
+	
+	Pet lab = 	information.getPet("Labrador Retriever"); 
+		
+		System.out.println(lab.breed);
 
 	}
 
---
+++ whileLoops/src/whileLoops/WhileLoops.java
@@ -0,0 +1,18 @@
+package whileLoops;
+
+public class WhileLoops {
+
+	public static void main(String[] args) {
+		
+		int x = 0;
+		while(x < 50) {
+			System.out.println("x is less than 50: " + x);
+			x++;
+		}
+		
+		
+		
+		
+	}
+
+}
