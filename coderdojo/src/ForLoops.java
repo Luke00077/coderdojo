14a9c8db5eb1c1862b37e8a3c66573d40ddae6a1
lukas <i.am.luke.c.b@gmail.com>
2021-04-14 20:19:46 -0700

    Commit

--- coderdojo/src/ForLoops.java
+++ coderdojo/src/ForLoops.java
@@ -2,10 +2,21 @@
 public class ForLoops {
 
 	public static void main(String[] args) {
-		for(int x = 0; x < 10; x++) {
+		// for (initialize; condition; updation) {
+		for (int x = 0; x < 10; x = x + 2) {
 			System.out.println("Hello world!");
 
+		}
+		for (int y = 0; y <= 10; y = y + 2) {
+			System.out.println(y);
+		}
+		for (int z = 0; z <= 50; z = z + 5) {
+			System.out.println(z);
+			if (z % 2 == 0) {
+				System.out.println("It is even.");
+			} else {
+				System.out.println("It's odd.");
+			}
+		}
 	}
-
-}
 }
--- whileLoops/src/whileLoops/WhileLoops.java
+++ whileLoops/src/whileLoops/WhileLoops.java
@@ -5,7 +5,7 @@ public class WhileLoops {
 	public static void main(String[] args) {
 		
 		int x = 0;
-		while(x < 50) {
+		while(true) {
 			System.out.println("x is less than 50: " + x);
 			x++;
 		}
