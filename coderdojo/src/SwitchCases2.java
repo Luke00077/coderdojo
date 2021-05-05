c84083eebf93d7c2461420607bf83b14f4aa5809
lukas <i.am.luke.c.b@gmail.com>
2021-05-05 13:25:25 -0700

    commit

---
+++ coderdojo/src/SwitchCases2.java
@@ -0,0 +1,35 @@
+
+public class SwitchCases2 {
+
+	public static void main(String[] args) {
+		
+		int age = 3;
+	switch (age) {
+	
+	case 1:
+		System.out.println("You might've said your first"
+				+ " word but you cannot walk");
+		break;
+	case 2: 
+		System.out.println("You can somewhat"
+				+ " talk and walk!");
+		break;
+	case 3: 
+		System.out.println("You can talk real well,"
+				+ " and you can write and doodle!");
+		break;
+	case 4: 
+		System.out.println("You go to school next year"
+				+ " and you're socially active!");
+		break;
+		default:
+			System.out.println("I don't how old you are!");
+		break;
+	
+	
+	}
+		
+
+	}
+
+}
